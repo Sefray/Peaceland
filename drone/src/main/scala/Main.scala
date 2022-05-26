adcc9a08aeaa2ce99d10b97670b3c360f51208a6
philippe.bernet <philippe.bernet@epita.fr>
2022-05-26 15:28:20 +0200

    feat: drone report : location

--- drone/src/main/scala/Main.scala
+++ drone/src/main/scala/Main.scala
@@ -13,20 +13,33 @@ import org.json4s._
 import org.json4s.jackson.Serialization
 import org.json4s.jackson.Serialization.write
 
-final case class DroneReport(droneId : Int)
+final case class Location(lat : String, lon : String)
+final case class Citizen(id : Int, score : Int, words : List[String])
+final case class DroneReport(id : Int, pos : Location, citizens : List[Citizen]) // TODO: Add timestamp
 
 object Main {
 
-  def generateDroneReport(rd : Random) : DroneReport = {
+  def generateLocation(rd : Random) : Location = {
+    def generateLocationIntern(max : Int, a : String, b : String) : String = {
+      val F = ((rd.nextDouble() * max - max / 2) * 100).round / 100f
+      val L = if (rd.nextInt() % 2 == 0) a else b
+      F.toString + " " + L
+    }
     
+    val lon = generateLocationIntern(180, "N", "S")
+    val lat = generateLocationIntern(360, "E", "W")
+
+    Location(lon, lat)
+  }
+
+  def generateDroneReport(rd : Random) : DroneReport = {
+
     val nb_drone = 20
 
     val droneId = rd.nextInt(nb_drone)
+    val location = generateLocation(rd)
 
-    
-
-
-    DroneReport(droneId)
+    DroneReport(droneId, location, citizens)
   }
 
   def main(args: Array[String]): Unit = {
