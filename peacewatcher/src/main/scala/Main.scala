4da97ea4e721e3900bc033422cebdde497e01777
Baptiste Bourdet <baptiste.bourdet@epita.fr>
2022-06-07 14:46:55 +0200

    feat(peacewatcher): dockerfile and logs

--- peacewatcher/src/main/scala/Main.scala
+++ peacewatcher/src/main/scala/Main.scala
@@ -17,21 +17,19 @@ object Main {
   def main(args: Array[String]): Unit = {
 
     val duration = 60
+    val host = scala.util.Properties.envOrElse("PL_KAFKA_HOST", "localhost:9092")
+    val nb_peacewatcher = scala.util.Properties.envOrElse("PL_NB_PEACEWATCHER", "2").toInt
+    val topic = scala.util.Properties.envOrElse("PL_REPORT_TOPIC", "reports")
 
     val props: Properties = new Properties()
-    props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092")
+    props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, host)
     props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, classOf[StringSerializer])
     props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, classOf[StringSerializer])
-
-    val producer: KafkaProducer[String, String] = new KafkaProducer[String, String](props)
-
-    val nb_peacewatcher = 2
-    val topic = "reports"
 
     List
     .range(0, nb_peacewatcher)
     .map(id => new Thread(new PeaceWatcher(id, topic, props)))
-    .foreach(t => t.start)
+    .foreach(t => t.start())
 
     Thread.sleep(duration * 1000)
   }
--- peacewatcher/src/main/scala/PeaceWatcher.scala
+++ peacewatcher/src/main/scala/PeaceWatcher.scala
@@ -2,22 +2,25 @@ package peaceland {
   package peacewatcher {
 
     import org.apache.kafka.clients.producer.{KafkaProducer, ProducerRecord}
+
     import java.time.Duration
     import java.util.Properties
     import scala.util.Random
     import java.util.UUID.randomUUID
-        
     import org.json4s._
     import org.json4s.jackson.Serialization
     import org.json4s.jackson.Serialization.write
-
     import com.github.nscala_time.time.Imports._
-
+    import org.slf4j.{Logger, LoggerFactory}
     import peaceland.peacewatcher.ReportGenerator.generateReport
 
     class PeaceWatcher(id: Int, topic: String, props: Properties)
-        extends Runnable {
-      def run() = {
+        extends Runnable  {
+
+      val logger: Logger = LoggerFactory.getLogger(s"PeaceWatcher$id")
+
+      def run(): Unit = {
+        logger.info("Start sending message")
         val intervention_duration = 30 // Seconds
         val poll_duration = 2
 
@@ -31,7 +34,7 @@ package peaceland {
         while (true) {
           Thread.sleep(100)
           if (rd.between(0, 100) == 42) {
-            println(s"$id generating report!")
+            logger.info("Sending message")
             producer.send(
               new ProducerRecord[String, String](
                 topic,
