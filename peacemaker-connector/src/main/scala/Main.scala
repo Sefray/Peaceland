5322a9f9c0a53c95d878ff79289d6dc2e2445410
Baptiste Bourdet <baptiste.bourdet@epita.fr>
2022-06-07 19:06:44 +0200

    feat(connector): working connector to discord

--- peacemaker-connector/src/main/scala/Main.scala
+++ peacemaker-connector/src/main/scala/Main.scala
@@ -1,26 +1,46 @@
-import org.apache.kafka.clients.consumer.{
-  KafkaConsumer,
-  ConsumerConfig,
-  ConsumerRecords
-}
-
-import org.apache.kafka.common.serialization.{StringDeserializer}
-
-import scala.collection.JavaConverters._
+import org.apache.http.HttpResponse
+import org.apache.http.client.methods.HttpPost
+import org.apache.http.entity.StringEntity
+import org.apache.http.impl.client.HttpClients
+import org.apache.http.util.EntityUtils
+import org.apache.kafka.clients.consumer.{ConsumerConfig, KafkaConsumer}
+import org.apache.kafka.common.serialization.StringDeserializer
+import org.json4s.DefaultFormats
+import org.json4s.jackson.{JsonMethods, Serialization}
+import org.slf4j.{Logger, LoggerFactory}
+import peaceland.peacewatcher.Alert
 
 import java.time.Duration
 import java.util.Properties
-
-import peaceland.peacemaker.PeaceMaker
-
-import org.json4s._
-import org.json4s.jackson.Serialization
-import org.json4s.jackson.Serialization.write
+import java.util.UUID.randomUUID
+import scala.collection.JavaConverters._
 
 object main {
+
+  def formatToString(alert: Alert): String = {
+    s"""
+       |:rotating_light::warning:**ALERT**:warning::rotating_light:
+       |
+       |A peacemaker:teddy_bear:needs to bring peace to:
+       |**${alert.citizen.id}**
+       |At https://www.google.com/maps/search/?api=1&query=${alert.pos.lat}%2C${alert.pos.lon}
+       |
+       |${
+      alert.citizen.score match {
+        case s if s > -25 => ":yellow_circle: Threat to peace level 1 :yellow_circle:"
+        case s if s > -60 => ":orange_circle: Threat to peace level 2 :orange_circle:"
+        case _ => ":red_circle: Threat to peace level 3 :red_circle:"
+      }
+    }
+       |
+       |:bouquet::cherry_blossom: Be at peace :hibiscus::bouquet:
+       |
+       |""".stripMargin
+  }
+
   def main(args: Array[String]): Unit = {
 
-    val duration = 60 // Second
+    implicit val formats: DefaultFormats.type = DefaultFormats
 
     val host = scala.util.Properties.envOrElse("PL_KAFKA_HOST", "localhost:9092")
     val topic = scala.util.Properties.envOrElse("PL_ALERT_TOPIC", "alerts")
@@ -34,32 +54,53 @@ object main {
     props.put(ConsumerConfig.GROUP_ID_CONFIG, group_id)
     props.put(ConsumerConfig.MAX_POLL_RECORDS_CONFIG, 1)
 
-    val intervention_duration = 5 // Seconds
     val poll_duration = 1
 
     val consumer: KafkaConsumer[String, String] = new KafkaConsumer[String, String](props)
     consumer.subscribe(List(topic).asJava)
 
-    val id = new Random().randomUUID()
-    val logger: Logger = LoggerFactory.getLogger(s"PeaceWatcher$id")
+    val id = randomUUID()
+    val logger: Logger = LoggerFactory.getLogger(s"PeaceMakerConnector$id")
 
-    def fun() : Unit = {
+    val client = HttpClients.createDefault()
+
+    def fun(): Unit = {
       val records = consumer.poll(Duration.ofSeconds(poll_duration))
-      records.asScala.foreach(record => {
-          logger.info(s"Sending alert!")
-          
-          val json_http = { "content" : record }
 
-          val result = Http(http_url).postData(json_http)
-          .header("Content-Type", "application/json")
-          .header("Charset", "UTF-8")
-          .option(HttpOptions.readTimeout(10000)).asString
+      records.asScala.foreach(f = record => {
+        logger.info(s"Sending alert!")
 
-          Thread.sleep(intervention_duration * 1000)
+        val record_json = JsonMethods.parse(record.value())
+        val alert: Alert = record_json.extract[Alert]
+
+        val content = formatToString(alert)
+        val json_object = Map("content" -> content)
+        val json_body: String = Serialization.write(json_object)
+        logger.info(json_body)
+
+        val httpPost = new HttpPost(http_url)
+        httpPost.addHeader("Content-Type", "application/json")
+        httpPost.addHeader("Charset", "UTF-8")
+        httpPost.setEntity(new StringEntity(json_body))
+
+
+        logger.info(httpPost.toString)
+
+        val result: HttpResponse = client.execute(httpPost);
+
+        logger.info(result.getStatusLine.getStatusCode.toString)
+
+
+        if (result.getStatusLine.getStatusCode < 300)
+          consumer.commitSync()
+        else
+          logger.error(s"Error ${result.getStatusLine.getStatusCode} on alert ${EntityUtils.toString(result.getEntity)}")
+
       })
-      consumer.commitSync()
+      Thread.sleep(5000L)
+      fun()
     }
 
-    Thread.sleep(duration * 1000)
+    fun()
   }
 }
---
+++ peacemaker-connector/src/main/scala/PeaceWatcherAlert.scala
@@ -0,0 +1,11 @@
+package peaceland {
+  package peacewatcher {
+    final case class AlertLocation(lat: String, lon: String)
+    final case class AlertCitizen(id: String, score: Int)
+    final case class Alert(
+                            pos: AlertLocation,
+                            citizen: AlertCitizen,
+                            timestamp: String
+                          )
+  }
+}
