d74ffefcc6ea4ac1a4de8896a94ad3ff2a7baf71
Marius Dubosc <marius@MariusLaptop.localdomain>
2022-06-09 21:30:57 +0200

    fix riskyZone

--- stats/src/main/scala/Main.scala
+++ stats/src/main/scala/Main.scala
@@ -1,12 +1,13 @@
 import org.apache.spark.sql.{DataFrame, SparkSession}
 import org.apache.spark.{SparkContext, SparkConf}
-import org.apache.spark.sql.types.{DataType, StructType, StringType, IntegerType, ArrayType, FloatType};
+import org.apache.spark.sql.types.{DataType, StructType, StringType, IntegerType, ArrayType, FloatType, LongType};
 import org.apache.hadoop.conf.Configuration
 import org.apache.hadoop.fs.{FileSystem, Path}
 import java.net.URI
 import org.apache.spark.sql.Row
 import org.apache.spark.sql.functions._
-import scala.collection.immutable._
+// import scala.collection.immutable._
+import scala.collection.JavaConversions._
 // import org.apache.spark.implicits._
 
 object Main{
@@ -32,7 +33,7 @@ object Main{
 		// avgNumberOfAlertPerDay(data);
 		// mostDangerousHour(data);
 		avgScoreOfRebelliousCitizen(flatten_data)
-		// mostRiskyZone(flatten_data)
+		riskyZone(flatten_data)
 	}
 
 	var suspicious_words = List("riot", "rebellion")
@@ -41,11 +42,14 @@ object Main{
 		!words.filter(word => suspicious_words.contains(word)).isEmpty
 	}
 
-	def avgScoreOfRebelliousCitizen(data : DataFrame) : Unit = {
-		data.where(array_contains(data("words"), "riot"))
+	def avgScoreOfRebelliousCitizen(data : DataFrame) : DataFrame = {
+		val df = data.where(array_contains(data("words"), "riot"))
 		.select(avg($"citizenScore"))
 		.withColumn("average score", $"avg(citizenScore)")
-		.drop($"avg(citizenScore)").show()
+		.drop($"avg(citizenScore)")
+
+		df.show(false)
+		return df
 	}
 
 	def mostDangerousHour(data : DataFrame) : Unit = {
@@ -58,10 +62,21 @@ object Main{
 
 	def getReportByZone(data : DataFrame, lat_min : Float, lat_max : Float, lon_min : Float, lon_max : Float) : DataFrame = {
 		val nb_report = data.filter($"lon" >= lon_min && $"lon" < lon_max && $"lat" >= lat_min && $"lat" < lat_max).count
-		return Seq(Row(lon_min, lon_max, lat_min, lat_max, nb_report)).toDF()
+
+		val schema = new StructType()
+		.add("lon_min",FloatType)
+		.add("lon_max",FloatType)
+		.add("lat_min",FloatType)
+		.add("lat_max",FloatType)
+		.add("suspiciois activities",LongType)
+
+		val df = spark.createDataFrame(Seq(Row(lon_min, lon_max, lat_min, lat_max, nb_report)), schema)
+
+		return df
+		// return spark.createDataFrame(Seq(Row(lon_min, lon_max, lat_min, lat_max, nb_report)), schema)
 	}
 
-	def mostRiskyZone(data : DataFrame, zones : List[List[Float]] = List(List(-180, 180, 0, 90), List(-180, 180, -90, 0)), threshold : Integer = 0) : Unit = {
+	def riskyZone(data : DataFrame, zones : List[List[Float]] = List(List(0, 90, -180, 180), List(-90, 0, -180, 180)), threshold : Integer = 0) : DataFrame = {
 		// val suspicious = data.filter($"words".filter(x => x.isin(suspicious_words: _*)).count() != 0)
 		val suspicious = data.filter($"citizenScore" < threshold)
 
@@ -70,10 +85,17 @@ object Main{
 		.add("lon_max",FloatType)
 		.add("lat_min",FloatType)
 		.add("lat_max",FloatType)
-		.add("suspicious activities",IntegerType)
+		.add("suspicious activities",FloatType)
 
-		val df = spark.createDataFrame(spark.sparkContext.emptyRDD[Row], schema)
-		val res = zones.foreach((lon_min : Float, lon_max : Float, lat_min : Float, lat_max : Float) => df.union(getReportByZone(suspicious, lat_min, lat_max, lon_min, lon_max)))
+		// return getReportByZone(suspicious, zones(0)(0), zones(0)(1),zones(0)(2),zones(0)(3))
+
+		var df = spark.createDataFrame(spark.sparkContext.emptyRDD[Row], schema)
+		// zones.foreach(coord : List[Float] => df.union(getReportByZone(suspicious, coord(0), coord(1),coord(2),coord(3))))
+		for (coord <- zones)
+			df = df.union(getReportByZone(suspicious, coord(0), coord(1),coord(2),coord(3)))
+		df.show(false)
+		return df
+		// val res = 
 	}
 }
 		
