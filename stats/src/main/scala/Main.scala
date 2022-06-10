20f92041df4a3ee233882372e7373e8f804704b4
Marius Dubosc <marius@MariusLaptop.localdomain>
2022-06-10 19:34:54 +0200

    fix(stats): rebellious citizen ok, top10 dangerous hours ok; removed useless prints

--- stats/src/main/scala/Main.scala
+++ stats/src/main/scala/Main.scala
@@ -1,9 +1,8 @@
 import org.apache.spark.sql.functions._
 import org.apache.spark.sql.types.{FloatType, LongType, StructType}
-import org.apache.spark.sql.{DataFrame, Row, SparkSession}
+import org.apache.spark.sql.{DataFrame, Row, SparkSession, Column}
 // import scala.collection.immutable._
 import scala.collection.JavaConversions._
-// import org.apache.spark.implicits._
 
 object Main {
   val sparkMaster: String = scala.util.Properties.envOrElse("PL_SPARK_MASTER", "local")
@@ -22,20 +21,24 @@ object Main {
 
 
     lazy val data = spark.read.option("inferSchema", "true").option("multiline", "true").format("json").load(s"${hdfsHost}${reportsGlob}")
-    data.show(false)
-    data.printSchema()
+    
+    // Uncomment the next lines to see the data and the schema loaded
+    // data.show(false)
+    // data.printSchema()
 
     val flatten_data = data.select($"*", explode($"citizens"))
       .drop($"citizens")
-      .withColumn("id", $"col.id")
+      .withColumn("citizenId", $"col.id")
       .withColumn("score", $"col.score")
       .withColumn("words", $"col.words")
       .drop($"col")
       .select($"*", $"pos.*")
       .drop($"pos")
-    // flatten_data.show(false)
-    // flatten_data.printSchema()
-    avgNumberOfAlertPerDay(flatten_data);
+
+    // Uncomment the next line if you want to see the data processed
+	  // flatten_data.show(false)
+	
+    avgNumberOfAlertPerDay(flatten_data)
     top10mostDangerousHours(flatten_data)
     avgScoreOfRebelliousCitizen(flatten_data)
     riskyZone(flatten_data)
@@ -43,37 +46,30 @@ object Main {
     spark.stop()
   }
 
-  var suspicious_words = List("riot", "rebellion")
 
-  def saySuspiciousWords(words: Array[String]): Boolean = {
-    !words.filter(word => suspicious_words.contains(word)).isEmpty
+  def avgScoreOfRebelliousCitizen(data: DataFrame): Unit = {
+	var suspicious_words = List("riot", "rebellion", "war", "dictator", "guns")
+    val df = data.select($"score", $"citizenId", explode($"words").as("word")) // Array spliting
+    .filter($"word".isin(suspicious_words:_*))  // filter on the suspicious words
+    .groupBy($"citizenId").avg("score") // removing the double suspicious words for one citizen
+    .select(avg($"avg(score)").as("Average score")) // get the average score
+	
+    println("The average score of rebellious citizen is:")
+    df.show()
   }
 
-  def avgScoreOfRebelliousCitizen(data: DataFrame): DataFrame = {
-    val df = data.where(array_contains(data("words"), "riot"))
-      .select(avg($"score"))
-      .withColumn("average score", $"avg(score)")
-      .drop($"avg(score)")
-    println("The average score of Rebellious Citizens is:")
-    df.show(false)
-    df
-  }
-
-  def top10mostDangerousHours(data: DataFrame, threshold: Integer = -50): Unit = {
+  def top10mostDangerousHours(data: DataFrame, threshold: Integer = 0): Unit = {
     val df = data.filter($"score" < threshold) // Get the alerts
-      .select(hour($"timestamp").as("hour")) // Select the hour
-      .groupBy("hour")
+      .select(substring(col("timestamp"), 0, 2).as("Dangerous hours")) // Select the hour
+      .groupBy("Dangerous hours")
       .count()
       .orderBy(desc("count")) // Order to get the most dangerous hours first
+    val count = df.count()
+    val nb = if (count > 10 ) 10 else count
+    println(f"The top ${nb} most dangerous hours are:")
 
-    println("The top 10 most dangerous hours are:")
-
-    df.select("hour").show(10)
+    df.select("Dangerous hours").show(10)
   }
-  // def coord_to_num(coord : StringType) : Unit = {
-  // 	val pattern = "([0-9]+.[0-9]+) [SNEO]"
-  // 	val pattern(num, card) = coord
-  // }
 
   def getReportByZone(data: DataFrame, lat_min: Float, lat_max: Float, lon_min: Float, lon_max: Float): DataFrame = {
     val nb_report = data.filter($"lon" >= lon_min && $"lon" < lon_max && $"lat" >= lat_min && $"lat" < lat_max).count
@@ -85,12 +81,10 @@ object Main {
       .add("lat_max", FloatType)
       .add("suspicious activities", LongType)
 
-    val df = spark.createDataFrame(Seq(Row(lon_min, lon_max, lat_min, lat_max, nb_report)), schema)
-    df
+    spark.createDataFrame(Seq(Row(lon_min, lon_max, lat_min, lat_max, nb_report)), schema)
   }
 
-  def riskyZone(data: DataFrame, zones: List[List[Float]] = List(List(0, 90, -180, 180), List(-90, 0, -180, 180)), threshold: Integer = 0): DataFrame = {
-    // val suspicious = data.filter($"words".filter(x => x.isin(suspicious_words: _*)).count() != 0)
+  def riskyZone(data: DataFrame, zones: List[List[Float]] = List(List(0, 90, -180, 180), List(-90, 0, -180, 180)), threshold: Integer = 0): Unit = {
     val suspicious = data.filter($"score" < threshold)
 
     val schema = new StructType()
@@ -100,27 +94,23 @@ object Main {
       .add("lat_max", FloatType)
       .add("suspicious activities", FloatType)
 
-    // return getReportByZone(suspicious, zones(0)(0), zones(0)(1),zones(0)(2),zones(0)(3))
-
     var df = spark.createDataFrame(spark.sparkContext.emptyRDD[Row], schema)
-    // zones.foreach(coord : List[Float] => df.union(getReportByZone(suspicious, coord(0), coord(1),coord(2),coord(3))))
     for (coord <- zones)
       df = df.union(getReportByZone(suspicious, coord(0), coord(1), coord(2), coord(3)))
     println("The riskiest zones are :")
     df.show(false)
-    df
   }
 
-  def avgNumberOfAlertPerDay(data : DataFrame,threshold: Integer = -50 ): Unit = {
+  def avgNumberOfAlertPerDay(data : DataFrame,threshold: Integer = 0 ): Unit = {
     val df = data.filter($"score" < threshold) // Get the alerts
       .select(date_trunc("day", $"timestamp").as("date")) // Select the date
       .groupBy("date")
       .count()
-      .select(avg($"count"))
+      .select(avg($"count").as("Number of alerts"))
 
     println("The average number of drone alerts per day is: ")
 
-    df.select("avg(count)").show()
+    df.select("Number of alerts").show()
   }
  }
 
